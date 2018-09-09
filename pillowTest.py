cef20216e4988c50385c1b824ee48b8ca35ea3c2
Jakaria Istauk <jakariamd35@gmail.com>
2018-09-09 13:54:59 +0600

    Add files via upload

---
+++ pillowTest.py
@@ -0,0 +1,4 @@
+from PIL import Image
+
+image1 = Image.open('artWork.png')
+image1.save()
---
+++ resize.py
@@ -0,0 +1,8 @@
+from PIL import Image
+import OS
+
+size_200 = (200,200)
+
+for f in os.listdir('.'):
+    if f.endswith('.png'):
+    
